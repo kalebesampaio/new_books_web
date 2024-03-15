e361b627f89a8396bcb3a8cf38629fd405786473
kalebesampaio <kalebesmp2002@gmail.com>
2024-03-15 11:06:24 -0300

    feat: criando o header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import useMedia from "use-media";
+import Logo from "../../../public/logo.png";
+import {
+  ButtonHeader,
+  Flex,
+  HeaderContainer,
+  HeaderMobileArea,
+  HeaderStyles,
+  Link,
+  LogoTipo,
+  NavbarLinks,
+  LogoTipoImg,
+} from "./styles";
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+
+export interface MenuButtonOpen {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+}
+export const Header = () => {
+  const isWide = useMedia({ maxWidth: "991px" });
+  const [open, setOpen] = useState(false);
+
+  const OpenMenu = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <HeaderStyles>
+      <HeaderContainer>
+        <HeaderMobileArea>
+          <LogoTipo>
+            <LogoTipoImg src={Logo} alt="" />
+          </LogoTipo>
+          {isWide && (
+            <ButtonHeader
+              onClick={OpenMenu}
+              aria-label={!open ? "Abrir Menu" : "Fechar Menu"}
+            >
+              {!open ? <FaBars /> : <IoClose />}
+            </ButtonHeader>
+          )}
+        </HeaderMobileArea>
+        <Flex>{isWide ? open && <NavLinks /> : <NavLinks />}</Flex>
+      </HeaderContainer>
+    </HeaderStyles>
+  );
+};
+
+export const NavLinks = (): JSX.Element => {
+  return (
+    <NavbarLinks>
+      <Link href={`#`}>Início</Link>
+      <Link href={`#`}>Mais Lidos</Link>
+      <Link href={`#`}>Tipos</Link>
+      <Link href={`#`}>Gêneros</Link>
+    </NavbarLinks>
+  );
+};
